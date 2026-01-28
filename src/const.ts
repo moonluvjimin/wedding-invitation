a5170a6d8e36f14aef85c91fae74396ddcd95254
moonluvjimin <96zb9tcrdz@privaterelay.appleid.com>
2026-01-29 04:32:44 +0900

    Refactor LOCATION and LOCATION_ADDRESS constants

--- src/const.ts
+++ src/const.ts
@@ -16,8 +16,8 @@ export const WEDDING_DATE_FORMAT = `YYYY년 MMMM D일 dddd A h시${WEDDING_DATE.
 // 예: 예식일 8월 -> 8월 15일 광복절
 export const HOLIDAYS = [5]
 
-export const LOCATION = "한국전력공사 남서울본부 9층 웨딩홀"
-export const LOCATION_ADDRESS = "서울시 영등포구 여의동로7길 5, 9층 웨딩홀"
+export const LOCATION = "한국전력공사남서울본부"
+export const LOCATION_ADDRESS = "서울시 영등포구 여의동로7길 5"
 
 // 카카오톡 공유 시 위치 정보로 사용할 주소.
 // LOCATION 과 동일하게 설정해도 무방하나, 필요에 따라 좀 더 상세히 작성 가능.
