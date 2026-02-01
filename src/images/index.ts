db62ea724adf9c70f924ff2c79921edf49dcc12a
moonluvjimin <96zb9tcrdz@privaterelay.appleid.com>
2026-02-02 01:45:15 +0900

    Replace old image imports with new ones

--- src/images/index.ts
+++ src/images/index.ts
@@ -2,29 +2,29 @@ import coverImage from "./wd1.jpg"
 import image1 from "./wd2.jpg"
 import image2 from "./wd3.jpg"
 import image3 from "./wd4.jpg"
-import image4 from "./image4.png"
-import image5 from "./image5.png"
-import image6 from "./image6.png"
-import image7 from "./image7.png"
-import image8 from "./image8.png"
-import image9 from "./image9.png"
-import image10 from "./image10.png"
-import image11 from "./image11.png"
-import image12 from "./image12.png"
+import image4 from "./wd5.jpg"
+import image5 from "./wd6.jpg"
+import image6 from "./wd7.jpg"
+import image7 from "./wd8.jpg"
+import image8 from "./wd9.jpg"
+import image9 from "./wd10.jpg"
+import image10 from "./wd11.jpg"
+import image11 from "./wd12.jpg"
+import image12 from "./wd13.jpg"
 
 export const COVER_IMAGE = coverImage
 
 export const GALLERY_IMAGES = [
-  image1,
-  image2,
-  image3,
-  image4,
-  image5,
-  image6,
-  image7,
-  image8,
-  image9,
-  image10,
-  image11,
-  image12,
+  wd1,
+  wd2,
+  wd3,
+  wd4,
+  wd5,
+  wd6,
+  wd7,
+  wd8,
+  wd9,
+  wd10,
+  wd11,
+  wd12,
 ]
